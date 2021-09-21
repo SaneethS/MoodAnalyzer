ed6eb013463ca484087db21e84a5a47ee35eb790
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-21 18:10:35 +0530

    UC1 is refactored to check for null values

--- src/test/java/com/moodanalyzertest/MoodAnalyzerTest.java
+++ src/test/java/com/moodanalyzertest/MoodAnalyzerTest.java
@@ -11,15 +11,22 @@ public class MoodAnalyzerTest {
 	
 	@Test
 	public void sadCheck() {
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		String message = moodAnalyzer.analyseMood("this is sad message");
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("this is sad message");
+		String message = moodAnalyzer.analyseMood();
 		Assert.assertEquals(message, "SAD");
 	}
 	
 	@Test
 	public void happyCheck() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer("this is happy message");
+		String message = moodAnalyzer.analyseMood();
+		Assert.assertEquals(message, "HAPPY");
+	}
+	
+	@Test
+	public void nullCheck() {
 		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		String message = moodAnalyzer.analyseMood("this is happy message");
+		String message = moodAnalyzer.analyseMood();
 		Assert.assertEquals(message, "HAPPY");
 	}
 }
